2a0513eab1d3432b62b0cd1c6621edccc26a5ea8
Murad <murad.jaddoon@gmail.com>
2022-07-19 12:38:58 +0500

    created video comp

---
+++ src/component/Video.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef } from "react";
+import thumbnail from "../assets/thumbnail.png";
+
+const Video = ({ src, controls, width, playVideo }) => {
+  const videoRef = useRef();
+  const playTheVideo = () => {
+    videoRef.current?.play();
+  };
+  const pauseTheVideo = () => {
+    videoRef.current?.pause();
+  };
+  console.log(playVideo);
+  if (playVideo) {
+    playTheVideo();
+  } else {
+    pauseTheVideo();
+  }
+  useEffect(() => {
+    videoRef.current?.load();
+  }, [src]);
+  return (
+    <div style={{ width: width || 240 }}>
+      <video
+        ref={videoRef}
+        width="100%"
+        loop
+        controls={controls || false}
+        poster={thumbnail}
+      >
+        <source src={src} type="video/mp4" />
+      </video>
+    </div>
+  );
+};
+
+export default Video;
