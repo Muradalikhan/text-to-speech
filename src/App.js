c15c0d53b0bf82a60b7f259c5bf6a2cbbffedec2
Murad <murad.jaddoon@gmail.com>
2022-07-19 12:39:36 +0500

    created app page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Bulb from "./component/bulb";
+import Dictaphone from "./component/speechToText";
+import Video from "./component/Video";
+import videoSrc from "./assets/video1.mp4";
 
 function App() {
+  const [switchOn, setSwitchOn] = useState(false);
+  const [bulbBreak, setBulbBreak] = useState(false);
+  const [playVideo, setPlayVideo] = useState(false);
   return (
     <div className="App">
-     <h1>Speech to text</h1>
+      <h1>Speech to text</h1>
+      <Dictaphone
+        setSwitchOn={setSwitchOn}
+        setBulbBreak={setBulbBreak}
+        setPlayVideo={setPlayVideo}
+      />
+      <div className="flex-box">
+        <Bulb switchOn={switchOn} Break={bulbBreak} />
+        <Video src={videoSrc} width="40%" controls playVideo={playVideo} />
+      </div>
     </div>
   );
 }
