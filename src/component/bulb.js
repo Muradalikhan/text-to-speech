b449ae8a017300426efe34613d5d6ad70a479837
Murad <murad.jaddoon@gmail.com>
2022-07-22 14:21:21 +0500

    integrated dark and light theme

--- src/component/bulb.js
+++ src/component/bulb.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React,{useContext} from "react";
 import bulbOn from "../assets/bulb-on.png";
 import bulbOff from "../assets/bulb-off.png";
 import bulbBreak from "../assets/bulb-break.png";
+import { ThemeContext } from "../App";
 
 const Bulb = ({ switchOn, Break }) => {
+  const {theme}=useContext(ThemeContext)
   return (
     <img
-      src={switchOn ? bulbOn : Break ? bulbBreak : bulbOff}
+      src={theme==='dark' ? bulbOn : bulbOff}
       width="300px"
       height="400px"
       alt="bulb-img"
