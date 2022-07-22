65ca1f8c8358895f03593d5ab4c836f50a8d1a0d
Murad <murad.jaddoon@gmail.com>
2022-07-22 14:21:56 +0500

    added switch comp

--- src/App.js
+++ src/App.js
@@ -4,25 +4,36 @@ import Bulb from "./component/bulb";
 import Dictaphone from "./component/speechToText";
 import Video from "./component/Video";
 import videoSrc from "./assets/video1.mp4";
+import Switch from "react-switch";
+import { BsSun, BsMoonStars } from "react-icons/bs";
 
 export const ThemeContext = createContext();
 
 function App() {
-  const [switchOn, setSwitchOn] = useState(false);
-  const [bulbBreak, setBulbBreak] = useState(false);
   const [playVideo, setPlayVideo] = useState(false);
+  const [checked, setChecked] = useState(false);
   const [theme, setTheme] = useState("light");
+  const handleChecked = () => {
+    setChecked((prev) => !prev);
+    setTheme(theme === "light" ? "dark" : "light");
+  };
   return (
-    <ThemeContext.Provider value={{theme, setTheme}}>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
       <div className="App" id={theme}>
         <h1>Speech to text</h1>
-        <Dictaphone
-          setSwitchOn={setSwitchOn}
-          setBulbBreak={setBulbBreak}
-          setPlayVideo={setPlayVideo}
+        <Dictaphone setPlayVideo={setPlayVideo} setChecked={setChecked} />
+        <Switch
+          onChange={handleChecked}
+          checked={checked}
+          checkedIcon={<BsSun fontSize="20px" className="checkedIcon" />}
+          uncheckedIcon={
+            <BsMoonStars fontSize="18px" className="checkedIcon" />
+          }
+          offColor="#223"
+          onColor="#14C9E4"
         />
         <div className="flex-box">
-          <Bulb switchOn={switchOn} Break={bulbBreak} />
+          <Bulb />
           <Video src={videoSrc} width="40%" controls playVideo={playVideo} />
         </div>
       </div>
