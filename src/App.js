197ae43211a503e25ea7e39dafdcc0a5b02cc273
Murad khan <murad.jaddoon@gmail.com>
2022-08-05 20:51:42 +0500

    integrated imgs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { useState, createContext } from "react";
 import { BsSun, BsMoonStars } from "react-icons/bs";
+import { ToastContainer } from "react-toastify";
 import Dictaphone from "./component/speechToText";
 import videoSrc from "./assets/video1.mp4";
 import Video from "./component/Video";
 import Bulb from "./component/bulb";
 import Switch from "react-switch";
+import waveUp from "./assets/wave-blue-up.svg";
+import waveDown from "./assets/wave-blue.svg";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 export const ThemeContext = createContext();
@@ -20,32 +24,39 @@ function App() {
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
       <div className="App" id={theme}>
-        <h1>Speech to text</h1>
-        <div className="flex">
-          <Dictaphone setPlayVideo={setPlayVideo} setChecked={setChecked} />
-          <Switch
-            onChange={handleChecked}
-            checked={checked}
-            checkedIcon={<BsSun fontSize="20px" className="checkedIcon" />}
-            uncheckedIcon={
-              <BsMoonStars fontSize="18px" className="checkedIcon" />
-            }
-            offColor="#223"
-            onColor="#14C9E4"
-          />
-        </div>
-        <div className="flex-box">
-          <Bulb />
-          <Video src={videoSrc} width="40%" controls playVideo={playVideo} />
+        <img src={waveUp} alt="blob" className="img-up" />
+        <div className="content">
+          <h1 className="heading">Speech to text</h1>
+          <div className="flex">
+            <Dictaphone setPlayVideo={setPlayVideo} setChecked={setChecked} />
+            <Switch
+              onChange={handleChecked}
+              checked={checked}
+              checkedIcon={<BsSun fontSize="20px" className="checkedIcon" />}
+              uncheckedIcon={
+                <BsMoonStars fontSize="18px" className="checkedIcon" />
+              }
+              offColor="#223"
+              onColor="#14C9E4"
+            />
+          </div>
+          <div className="flex-box">
+            <Bulb />
+            <Video src={videoSrc} width="40%" controls playVideo={playVideo} />
+          </div>
         </div>
         <div className="commands">
           <h3>Commands</h3>
           <ul>
             <li>Clear</li>
-            <li>light mode</li>
+            <li>play</li>
+            <li>pause</li>
+            <li>dark mode</li>
             <li>dark mode</li>
           </ul>
         </div>
+        <ToastContainer autoClose={1000} />
+        <img src={waveDown} alt="blob" className="img-down" />
       </div>
     </ThemeContext.Provider>
   );
