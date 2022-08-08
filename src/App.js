138110eb192145bca92783a098dec381fee1f0a3
Murad khan <murad.jaddoon@gmail.com>
2022-08-08 14:06:39 +0500

    updated layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useState, createContext } from "react";
-import { BsSun, BsMoonStars } from "react-icons/bs";
 import { ToastContainer } from "react-toastify";
-import Dictaphone from "./component/speechToText";
+import Dictaphone from "./component/SpeechToText";
 import videoSrc from "./assets/video1.mp4";
 import Video from "./component/Video";
-import Bulb from "./component/bulb";
-import Switch from "react-switch";
+import Bulb from "./component/Bulb";
 import waveUp from "./assets/wave-blue-up.svg";
+import waveUpOrange from "./assets/wave-orange-up.svg";
 import waveDown from "./assets/wave-blue.svg";
+import waveDownOrange from "./assets/wave-orange.svg";
+import ThemeSwitch from "./component/Switch";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
@@ -24,21 +25,12 @@ function App() {
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
       <div className="App" id={theme}>
-        <img src={waveUp} alt="blob" className="img-up" />
+        <img src={theme==="light"?waveUp:waveUpOrange} alt="blob" className="img-up" />
         <div className="content">
           <h1 className="heading">Speech to text</h1>
           <div className="flex">
             <Dictaphone setPlayVideo={setPlayVideo} setChecked={setChecked} />
-            <Switch
-              onChange={handleChecked}
-              checked={checked}
-              checkedIcon={<BsSun fontSize="20px" className="checkedIcon" />}
-              uncheckedIcon={
-                <BsMoonStars fontSize="18px" className="checkedIcon" />
-              }
-              offColor="#223"
-              onColor="#14C9E4"
-            />
+            <ThemeSwitch handleChecked={handleChecked} checked={checked} />
           </div>
           <div className="flex-box">
             <Bulb />
@@ -52,11 +44,11 @@ function App() {
             <li>play</li>
             <li>pause</li>
             <li>dark mode</li>
-            <li>dark mode</li>
+            <li>light mode</li>
           </ul>
         </div>
         <ToastContainer autoClose={1000} />
-        <img src={waveDown} alt="blob" className="img-down" />
+        <img src={theme==="light"?waveDown:waveDownOrange} alt="blob" className="img-down" />
       </div>
     </ThemeContext.Provider>
   );
